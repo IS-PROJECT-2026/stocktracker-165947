5cbf5908ce6fc091978af563fb719fd6f46d98de
KiruiRG <gloria.rutoh@strathmore.edu>
2026-08-17 23:15:16 +0300

    feat: build stock dashboard

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("StockTracker loaded successfully.");
+const addProductButton = document.getElementById("addProductBtn");
+
+addProductButton.addEventListener("click", function () {
+    alert("Product management will be available in the next development phase.");
+});
+
+console.log("StockTrack dashboard loaded successfully.");
